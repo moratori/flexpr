40373a6080d47813aa0318d99edebf52cfe43344
moratori <hoko1993@yahoo.co.jp>
2014-01-29 23:44:33 +0900

    normalization

--- util.lisp
+++ util.lisp
@@ -4,7 +4,9 @@
 (ns:defns util
 	(:use :cl
 		  :constant
-		  :struct))
+		  :struct)
+	(:import-from :optima
+				  :match))
 
 @export
 (defmethod opr->strength ((opr operator))
@@ -19,6 +21,31 @@
 (defmethod opr-equal? ((opr1 operator) (opr2 operator))
   (eq (operator-opr opr1)
 	  (operator-opr opr2)))
+
+
+
+
+;;; normalization 
+
+
+;;; 量化子を先頭に移動(冠頭形に変換)
+@export
+(defgeneric lexpr->prenex (a)
+	(:documentation "prenex normal formalizetion"))
+
+
+;;; スコーレム標準形にする
+@export
+(defgeneric lexpr->skolem (a)
+	(:documentation "skolemization"))
+
+
+;;; 節形式に変換する(連言標準形(CNF) (L V L ...) & (L V L ...) & ... (L V L ...) )
+@export
+(defgeneric lexpr->clause-form (a)
+	(:documentation "clause formalization"))
+
+
 
 
 
